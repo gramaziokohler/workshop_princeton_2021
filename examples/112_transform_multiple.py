4de183d09762c53ab70d9c39f117ed26eeaf3c60
Gonzalo Casas <casas@arch.ethz.ch>
2021-06-29 14:21:14 +0200

    typo

--- examples/112_transform_multiple.py
+++ examples/112_transform_multiple.py
@@ -16,9 +16,9 @@ T = Rotation.from_axis_and_angle([-0.248, -0.786, -0.566], 2.78, point=[1.0, 0.0
 # transform points with transform_points
 t0 = time.time()
 transform_points(v, T)
-print("transfrom_points takes {:.4f} seconds.".format(time.time() - t0))
+print("transform_points takes {:.4f} seconds.".format(time.time() - t0))
 
 # transform points with transform_points_numpy
 t0 = time.time()
 transform_points_numpy(v, T)
-print("transfrom_points_numpy takes {:.4f} seconds.".format(time.time() - t0))
+print("transform_points_numpy takes {:.4f} seconds.".format(time.time() - t0))
